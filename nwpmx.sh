8a7d364d50af6fd90fac2211538e2d7f4cde95bb
Klaus Eder (hax) <k.eder@ederit.com>
2024-06-12 18:19:56 +0200

    adapt for nw

--- nwpmx.sh
+++ nwpmx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://github.com/Weilbyte/nwpmx
+# https://github.comederitsolutionsnwpmx
 
 umask 022
 
@@ -87,7 +87,7 @@ function checkSupported {
         local SUPPORTEDARR=($(echo "$SUPPORTED" | tr ',' '\n'))
         if ! (printf '%s\n' "${SUPPORTEDARR[@]}" | grep -q -P "$PVEVersionMajor"); then
             echo -e "${WARN}You might encounter issues because your version ($PVEVersionMajor) is not matching currently supported versions ($SUPPORTED)."
-            echo -e "If you do run into any issues on >newer< versions, please consider opening an issue at https://github.com/Weilbyte/nwpmx/issues.${REG}"
+            echo -e "If you do run into any issues on >newer< versions, please consider opening an issue at https://github.comederitsolutionsnwpmx/issues.${REG}"
         fi
     fi
 }
@@ -121,7 +121,7 @@ function usage {
         echo -e "  0                    OK"
         echo -e "  1                    Failure"
         echo -e "  2                    Already installed, OR not installed (when using install/uninstall commands)\n"
-        echo -e "Report issues at: <https://github.com/Weilbyte/nwpmx/issues>"
+        echo -e "Report issues at: <https://github.comederitsolutionsnwpmx/issues>"
     fi
 }
 
